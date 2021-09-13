6cc287cead670ee517a774e1a3c478cc54bb2b98
Vitaliy Viktorovich <po4tavela@gmail.com>
2021-09-13 19:58:34 +0300

    Added cloud animation to loading screen

--- VKApp_TalalayVV/Controllers/LoadingViewController.swift
+++ VKApp_TalalayVV/Controllers/LoadingViewController.swift
@@ -22,9 +22,14 @@ class LoadingViewController: UIViewController {
         self.leftDot.alpha = 0
         self.rightDot.alpha = 0
         self.middleDot.alpha = 0
+        // Задаем цвет точкам
+        self.leftDot.tintColor = .systemBackground
+        self.rightDot.tintColor = .systemBackground
+        self.middleDot.tintColor = .systemBackground
         // Вызываем метод анимации
         showDotsAnimation()
 //        showDotsAnimation2()
+        showCloudAnimation()
     }
     
     /// Метод анимирующий точки на экране
@@ -133,5 +138,84 @@ class LoadingViewController: UIViewController {
                     completion: nil)
             }
         }
+    }
+    
+    /// Анимация облачка
+    func showCloudAnimation(){
+        
+        /// Вьюха облачка
+        let cloudView = UIView()
+        
+        // Добавляем в сабвью
+        view.addSubview(cloudView)
+        // Отключаем авторесайз
+        cloudView.translatesAutoresizingMaskIntoConstraints = false
+        // Задаем констрейнты
+        NSLayoutConstraint.activate([
+            cloudView.centerXAnchor.constraint(
+                equalTo: view.centerXAnchor),
+            cloudView.bottomAnchor.constraint(
+                equalTo: middleDot.topAnchor,
+                constant: -10),
+            cloudView.widthAnchor.constraint(
+                equalToConstant: 120),
+            cloudView.heightAnchor.constraint(
+                equalToConstant: 70)
+        ])
+        /// Форма облачка
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 10, y: 60))
+        path.addQuadCurve(
+            to: CGPoint(x: 20, y: 40),
+            controlPoint: CGPoint(x: 5, y: 50))
+        path.addQuadCurve(
+            to: CGPoint(x: 40, y: 20),
+            controlPoint: CGPoint(x: 20, y: 20))
+        path.addQuadCurve(
+            to: CGPoint(x: 70, y: 20),
+            controlPoint: CGPoint(x: 55, y: 0))
+        path.addQuadCurve(
+            to: CGPoint(x: 80, y: 30),
+            controlPoint: CGPoint(x: 80, y: 20))
+        path.addQuadCurve(
+            to: CGPoint(x: 110, y: 60),
+            controlPoint: CGPoint(x: 110, y: 30))
+        path.close()
+
+        /// Слой для анимации облачка
+        let layerAnimation = CAShapeLayer()
+        layerAnimation.path = path.cgPath
+        layerAnimation.strokeColor = UIColor.systemBackground.cgColor
+        layerAnimation.fillColor = UIColor.clear.cgColor
+        layerAnimation.lineWidth = 8
+        layerAnimation.lineCap = .round
+        // Добавляем слой
+        cloudView.layer.addSublayer(layerAnimation)
+        
+        /// Анимация в конце пути
+        let pathAnimationEnd = CABasicAnimation(keyPath: "strokeEnd")
+        pathAnimationEnd.fromValue = 0
+        pathAnimationEnd.toValue = 1
+        pathAnimationEnd.duration = 1.5
+        pathAnimationEnd.fillMode = .both
+        pathAnimationEnd.isRemovedOnCompletion = false
+        
+        /// Анимация в начале пути
+        let pathAnimationStart = CABasicAnimation(keyPath: "strokeStart")
+        pathAnimationStart.fromValue = 0
+        pathAnimationStart.toValue = 1
+        pathAnimationStart.duration = 1.5
+        pathAnimationStart.fillMode = .both
+        pathAnimationStart.isRemovedOnCompletion = false
+        pathAnimationStart.beginTime = 1
+        
+        /// Группа анимаций для начала и конца
+        let animationGroup = CAAnimationGroup()
+        animationGroup.duration = 2.5
+        animationGroup.fillMode = CAMediaTimingFillMode.backwards
+        animationGroup.animations = [pathAnimationEnd, pathAnimationStart]
+        animationGroup.repeatCount = .infinity
+        layerAnimation.add(animationGroup, forKey: nil)
+
     }
 }
